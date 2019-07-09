3e4e6ff62f49cf69e2233fb0c2d57f9d979079f8
christianavdm <christianavdm@gmail.com>
2019-07-09 14:56:09 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :travelers
-  resources :universes
-  resources :creatures
+  resources :universes do 
+    resources :creatures
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
