9f51a9a244f579a46bafdb3a512aed8b1da89d26
christianavdm <christianavdm@gmail.com>
2019-07-09 16:29:10 -0700

    Travelers controller new and create actions

--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,2 +1,12 @@
 class TravelersController < ApplicationController
+    def create
+        Traveler.create(traveler_params)
+    end
+     
+    private
+    
+    def traveler_params
+    params.require(:traveler).permit(:name, :uid, :username, :password, :password_confirmation)
+    end
+
 end
