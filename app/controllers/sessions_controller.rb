f00cb65fe85daab684d72285824b553a6b8422b4
christianavdm <christianavdm@gmail.com>
2019-07-09 16:42:33 -0700

    Edited Sessions log in paths

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
+    
+    def new 
+    end
+
     def create
         @traveler = Traveler.find_by(username: params[:username])
         return head(:forbidden) unless @traveler.authenticate(params[:password])
         session[:traveler_id] = @traveler.id
+        redirect_to @traveler
     end
+
+    def destroy
+        session.delete :traveler_id
+        redirect_to controller: 'application', action: 'welcome'
+      end
 end
--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,12 +1,19 @@
 class TravelersController < ApplicationController
+    def new 
+        @traveler = Traveler.new
+    end 
+
     def create
         Traveler.create(traveler_params)
+    end
+
+    def show 
     end
      
     private
     
     def traveler_params
-    params.require(:traveler).permit(:name, :uid, :username, :password, :password_confirmation)
+        params.require(:traveler).permit(:name, :uid, :username, :password, :password_confirmation)
     end
 
 end
