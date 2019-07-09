dafba060ddb080fb88d79baf6ebff1b350d277b5
christianavdm <christianavdm@gmail.com>
2019-07-09 14:34:24 -0700

    Added routes and view files to Universe

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,2 +1,50 @@
 class UniversesController < ApplicationController
+
+    def index
+        @universes = Universe.all
+    end
+    
+    def show
+        @universe = Universe.find(params[:id])
+    end
+    
+    def new
+        @universe = Universe.new
+    end
+    
+    def edit
+        @universe = Universe.find(params[:id])
+    end
+    
+    def create
+        @universe = Universe.new(universe_params)
+        if @universe.save
+            redirect_to @universe
+        else
+            render :new
+        end
+    end
+    
+    def update
+        @universe = Universe.find(params[:id])
+        if @universe.update(universe_params)
+            redirect_to @universe
+        else
+            render :edit
+        end
+    end
+    
+    def destroy
+        @universe = Universe.find(params[:id])
+        @universe.destroy
+        redirect_to universes_url
+    end
+    
+    private
+    
+    def universe_params
+        params.require(:universe).permit(
+        :name, :description
+        )
+    end
 end
