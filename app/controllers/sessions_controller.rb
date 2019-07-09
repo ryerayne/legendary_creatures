f56f41b7b4373a591b4624d0ea9ef83aa1b3752d
christianavdm <christianavdm@gmail.com>
2019-07-09 16:38:43 -0700

    Added welcome page and updated routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,7 @@
 class SessionsController < ApplicationController
+    def create
+        @traveler = Traveler.find_by(username: params[:username])
+        return head(:forbidden) unless @traveler.authenticate(params[:password])
+        session[:traveler_id] = @traveler.id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :universes do 
     resources :creatures
   end
+
+  root :to => 'application#welcome'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
+ # post '/users/ride' => 'users#ride'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
