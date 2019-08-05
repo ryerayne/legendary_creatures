f434ea8c400fb9acec7a6481416a5b3e2ed1aaf4
christianavdm <christianavdm@gmail.com>
2019-08-05 12:06:36 -0700

    Added description JSON

--- app/assets/javascripts/universes.js
+++ app/assets/javascripts/universes.js
@@ -1,6 +1,10 @@
 $(document).on('turbolinks:load', function() {
     $(".js-more").on('click', function(event) {
         const id = $(this).data("id");
-        console.log(id); 
+        $.get("/universes/" + id + ".json", function(data) {
+            const universe = data; 
+            const description = "<p>" + universe["description"] + "</p>" 
+            $("#universe-" + id).html(description);
+        });
     });
 });
