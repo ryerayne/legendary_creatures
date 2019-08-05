25ec6a7697acfad711b98439437c817ea39e0662
christianavdm <christianavdm@gmail.com>
2019-08-05 15:20:30 -0700

    Added prototype and model functions

--- app/assets/javascripts/universes.js
+++ app/assets/javascripts/universes.js
@@ -4,6 +4,11 @@ function Universe(id, name, description) {
     this.description = description;
 }
 
+Universe.prototype.createDescription = function() {
+    let description = "<p>" + this.description + "</p>"; 
+    return description; 
+};
+
 Universe.prototype.createLink = function() {
     let link = "<a href='/universes/" + this.id + "'>Visit Universe</a>"; 
     return link; 
@@ -13,10 +18,11 @@ $(document).on('turbolinks:load', function() {
     $(".js-more").on('click', function(event) {
         const id = $(this).data("id");
         $.get("/universes/" + id + ".json", function(data) {
-            const universe = data; 
-            const description = "<p>" + universe["description"] + "</p>" 
-            const universeLink = "<a href='/universes/" + id + "'>Visit Universe</a>"
-            $("#universe-" + id).html(description + universeLink);
+            const universeData = data; 
+            let newUniverse = new Universe(id, universeData["name"], universeData["description"]); 
+            let description = newUniverse.createDescription(); 
+            let link = newUniverse.createLink(); 
+            $("#universe-" + id).html(description + link);
         });
     });
 
@@ -27,9 +33,9 @@ $(document).on('turbolinks:load', function() {
         let posting = $.post('/universes', values);
 
         posting.done(function(data) {
-            let universeName = data.name; 
+            let newUniverse = new Universe(data.id, data.name, data.description)
             let universeDescription = data.description; 
-            let newUniverse = "<h3>" + universeName + "</h3>"
+            let newUniverse = "<h3>" + newUniverse.name + "</h3>"
             $("#new-universe").html(newUniverse); 
           });
     });
