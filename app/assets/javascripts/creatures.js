128c03afc09fe142ad85a0851b1b77eb7b801174
christianavdm <christianavdm@gmail.com>
2019-08-07 13:19:49 -0700

    Added sort button

--- app/assets/javascripts/creatures.js
+++ app/assets/javascripts/creatures.js
@@ -13,4 +13,32 @@ $(document).on('turbolinks:load', function() {
             $("#creatures-list").html(creaturesList);
         });
     });
+
+    $(".sort-creatures").on('click', function(event) {
+        const id = $(this).data("id");
+        $.get("/universes/" + id + ".json", function(data) {
+            const universeData = data; 
+            let creatures = universeData.creatures; 
+
+            creatures.sort(function(a, b) {
+                var nameA = a.name.toUpperCase(); 
+                var nameB = b.name.toUpperCase(); 
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+                return 0;
+              });
+
+            let creaturesList = "<h3>Creatures<h3>";
+            creatures.forEach(function(creature) {
+              creaturesList += '<h4>' + creature.name + '</h4><p>' + creature.description + '</p>'
+              creaturesList += "<a href='/universes/" + id + "/creatures/" + creature.id + "'>Meet Creature</a><br>"
+            });
+
+            $("#creatures-list").html(creaturesList);
+        });
+    });
 });
