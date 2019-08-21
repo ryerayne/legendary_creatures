116b4a56ef67f2001b3279fc48868cdf12e13199
christianavdm <christianavdm@gmail.com>
2019-08-20 18:34:20 -0700

    Comment features

--- app/assets/javascripts/universes.js
+++ app/assets/javascripts/universes.js
@@ -10,30 +10,30 @@ Universe.prototype.createDisplayHtml = function() {
     return description + link; 
 };
 
-$(document).on('turbolinks:load', function() {
-    $(".js-more").on('click', function(event) {
-        const id = $(this).data("id");
-        $.get("/universes/" + id + ".json", function(data) {
-            const universeData = data; 
-            let newUniverse = new Universe(id, universeData["name"], universeData["description"]); 
-            let displayHtml = newUniverse.createDisplayHtml(); 
-            $("#universe-" + id).html(displayHtml);
-        });
-    });
+// $(document).on('turbolinks:load', function() {
+//     $(".js-more").on('click', function(event) {
+//         const id = $(this).data("id");
+//         $.get("/universes/" + id + ".json", function(data) {
+//             const universeData = data; 
+//             let newUniverse = new Universe(id, universeData["name"], universeData["description"]); 
+//             let displayHtml = newUniverse.createDisplayHtml(); 
+//             $("#universe-" + id).html(displayHtml);
+//         });
+//     });
 
-    $("form").on('submit', function(event) {
-        event.preventDefault();
-        let values = $(this).serialize();
+//     $("form").on('submit', function(event) {
+//         event.preventDefault();
+//         let values = $(this).serialize();
  
-        let posting = $.post('/universes', values);
+//         let posting = $.post('/universes', values);
 
-        posting.done(function(data) {
-            let newUniverse = new Universe(data.id, data.name, data.description)
-            let newUniverseName = "<h3>" + newUniverse.name + "</h3>"
-            let displayHtml = newUniverse.createDisplayHtml(); 
+//         posting.done(function(data) {
+//             let newUniverse = new Universe(data.id, data.name, data.description)
+//             let newUniverseName = "<h3>" + newUniverse.name + "</h3>"
+//             let displayHtml = newUniverse.createDisplayHtml(); 
             
-            $("#new-universe").html(newUniverseName + displayHtml); 
-          });
-    });
+//             $("#new-universe").html(newUniverseName + displayHtml); 
+//           });
+//     });
 
-});
+// });
